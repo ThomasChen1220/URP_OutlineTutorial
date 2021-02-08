32868df83fabe49961a1815399ccf5b7b304353b
canchen <canchen@usc.edu>
2021-02-08 23:01:08 +0800

    fresnel outline done

---
+++ Assets/Shader/custom/GetMainLightDirection.hlsl
@@ -0,0 +1,13 @@
+﻿#ifndef GETMAINLIGHTDIRECTION_INCLUDED
+#define GETMAINLIGHTDIRECTION_INCLUDED
+
+void GetMainLightDirection_float(out float3 Out)
+{
+#ifdef SHADERGRAPH_PREVIEW
+	Out = float3(-1, 0, 0);
+#else 
+	Out = GetMainLight().direction;
+#endif
+}
+
+#endif
